9ed0129d19eddadf18ee4ffb785725c73abaee30
Ethan <ethan.frey.2020@stuent.lhschools.org>
2019-12-05 10:31:15 -0500

    Max Completed

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -14,6 +14,7 @@ public class Main {
         printArray(array);
         System.out.println("The sum of the array is " + sum(array));
         System.out.println("The min of the array is " + min(array));
+        System.out.println("The max of the array is " +max(array));
 
         Pattern1();
         Pattern2();
@@ -66,6 +67,14 @@ public class Main {
             if (array[i] < min) min = array[i];
         }
         return min;
+    }
+
+    public static int max(int[] array){
+        int max = array[0];
+        for (int i=0; i > array.length; i++){
+            if (array[i]>max) max = array[i];
+        }
+        return max;
     }
 
 
